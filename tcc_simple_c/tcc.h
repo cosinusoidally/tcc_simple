b283b4020d430247b0e0eb7a31b9d33c9dab7000
Liam Wilson <cosinusoidally@gmail.com>
2024-12-07 20:59:20 +0000

    more

--- tcc_simple_c/tcc.h
+++ tcc_simple_c/tcc.h
@@ -126,7 +126,6 @@ enum {
 /* token symbol management */
 typedef struct TokenSym {
     struct TokenSym *hash_next;
-    struct Sym *sym_struct; /* direct pointer to structure */
     struct Sym *sym_identifier; /* direct pointer to identifier */
     int tok; /* token number */
     int len;
