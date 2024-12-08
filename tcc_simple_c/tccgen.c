71fd958b8450a7bfcbc84323778ec52bd1090aeb
Liam Wilson <cosinusoidally@gmail.com>
2024-12-08 17:51:23 +0000

    more

--- tcc_simple_c/tccgen.c
+++ tcc_simple_c/tccgen.c
@@ -601,10 +601,10 @@ static void gen_opic(int op)
     uint64_t l1 = c1 ? v1->c.i : 0;
     uint64_t l2 = c2 ? v2->c.i : 0;
 
-    if ((PTR_SIZE != 8 || t1 != VT_PTR))
+    if (t1 != VT_PTR)
         l1 = ((uint32_t)l1 |
               (v1->type.t & VT_UNSIGNED ? 0 : -(l1 & 0x80000000)));
-    if ((PTR_SIZE != 8 || t2 != VT_PTR))
+    if (t2 != VT_PTR)
         l2 = ((uint32_t)l2 |
               (v2->type.t & VT_UNSIGNED ? 0 : -(l2 & 0x80000000)));
 
@@ -614,7 +614,7 @@ static void gen_opic(int op)
             l1 = l1 != l2;
             break;
         }
-	if ((PTR_SIZE != 8 || t1 != VT_PTR))
+	if (t1 != VT_PTR)
 	    l1 = ((uint32_t)l1 |
 		(v1->type.t & VT_UNSIGNED ? 0 : -(l1 & 0x80000000)));
         v1->c.i = l1;
