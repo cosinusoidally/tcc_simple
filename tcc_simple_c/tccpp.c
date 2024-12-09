490482327a09e6227a0500e782b498c90a5075b8
Liam Wilson <cosinusoidally@gmail.com>
2024-12-09 07:54:44 +0000

    more

--- tcc_simple_c/tccpp.c
+++ tcc_simple_c/tccpp.c
@@ -646,22 +646,7 @@ static inline void next_nomacro1(void)
     }
 
     c = *p;
-    switch(c) {
-    case 'a': case 'b': case 'c': case 'd':
-    case 'e': case 'f': case 'g': case 'h':
-    case 'i': case 'j': case 'k': case 'l':
-    case 'm': case 'n': case 'o': case 'p':
-    case 'q': case 'r': case 's': case 't':
-    case 'u': case 'v': case 'w': case 'x':
-    case 'y': case 'z': 
-    case 'A': case 'B': case 'C': case 'D':
-    case 'E': case 'F': case 'G': case 'H':
-    case 'I': case 'J': case 'K': 
-    case 'M': case 'N': case 'O': case 'P':
-    case 'Q': case 'R': case 'S': case 'T':
-    case 'U': case 'V': case 'W': case 'X':
-    case 'Y': case 'Z': 
-    case '_':
+    if(isid(c)) {
         p1 = p;
         h = TOK_HASH_INIT;
         h = TOK_HASH_FUNC(h, c);
@@ -704,7 +689,8 @@ static inline void next_nomacro1(void)
             ts = tok_alloc(tokcstr.data, tokcstr.size);
         }
         tok = ts->tok;
-        break;
+    } else {
+    switch(c) {
     case '0': case '1': case '2': case '3':
     case '4': case '5': case '6': case '7':
     case '8': case '9':
@@ -775,6 +761,7 @@ static inline void next_nomacro1(void)
         tok = c;
         p++;
         break;
+    }
     }
     tok_flags = 0;
     file->buf_ptr = p;
