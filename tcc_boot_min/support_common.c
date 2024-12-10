436e7f4711d81eb1d0b5f8e4c69834fef67a8d89
Liam Wilson <cosinusoidally@gmail.com>
2024-12-10 12:32:42 +0000

    more

--- tcc_boot_min/support_common.c
+++ tcc_boot_min/support_common.c
@@ -17,5 +17,34 @@ int wi32(int o, int v) {
   wi8(add(o, 3), and(v, 0xFF));
 }
 
+int v_esp; int v_ebp; int v_stack_size; int v_stack;
+
+int init_c(){
+  puts("init_c called");
+  v_stack_size=mul(64,1024);
+  v_stack=calloc(1,v_stack_size);
+  v_esp=sub(add(v_stack,v_stack_size),4);
+  v_ebp=v_esp;
+}
+
+int v_alloca(int x) {
+  v_esp=sub(v_esp, x);
+  return v_esp;
+}
+
+int enter() {
+/* FIXME detect overflow */
+  v_esp=sub(v_esp, 4);
+  wi32(v_esp,v_ebp);
+  v_ebp=v_esp;
+}
+
+int leave(int x) {
+  v_esp=v_ebp;
+  v_ebp=ri32(v_esp);
+  v_esp=add(v_esp, 4);
+  return x;
+}
+
 /* ================= end support_common.c ================================= */
 
