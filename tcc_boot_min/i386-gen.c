4f5431cda698b0ab83739ca298b1632cae78e435
Liam Wilson <cosinusoidally@gmail.com>
2024-12-11 17:58:37 +0000

    more

--- tcc_boot_min/i386-gen.c
+++ tcc_boot_min/i386-gen.c
@@ -147,9 +147,10 @@ int gadd_sp(int val) {
 /* 'is_jmp' is '1' if it is a jump */
 int gcall_or_jmp(int is_jmp) {
     int r;
+
     /* constant and relocation case */
-    greloc(cur_text_section, vtop->sym, ind + 1, R_386_PC32);
-    oad(0xe8 + is_jmp, vtop->c.i - 4); /* call/jmp im */
+    greloc(cur_text_section, vtop->sym, add(ind, 1), R_386_PC32);
+    oad(add(0xe8, is_jmp), vtop->c.i - 4); /* call/jmp im */
 }
 
 /* Generate function call. The function address is pushed first, then
