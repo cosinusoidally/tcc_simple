b44aa834b1145310ef9200d97857893580b370b1
Liam Wilson <cosinusoidally@gmail.com>
2024-12-12 22:46:20 +0000

    more

--- tcc_boot_min/tcc_boot_min.c
+++ tcc_boot_min/tcc_boot_min.c
@@ -103,6 +103,22 @@ int gsym_next(int o) {
 
 /* BufferedFile accessors */
 
+int gbf_buf_ptr(int o) {
+  return ri32(add(o, BufferedFile_buf_ptr_o));
+}
+
+int sbf_buf_ptr(int o, int v) {
+  wi32(add(o, BufferedFile_buf_ptr_o), v);
+}
+
+int gbf_buf_end(int o) {
+  return ri32(add(o, BufferedFile_buf_end_o));
+}
+
+int sbf_buf_end(int o, int v) {
+  wi32(add(o, BufferedFile_buf_end_o), v);
+}
+
 int gbf_fd(int o) {
   return ri32(add(o, BufferedFile_fd_o));
 }
