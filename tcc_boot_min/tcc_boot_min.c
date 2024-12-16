cc2586ca26bc3c1b67eeef1eb94fa8519aa759c4
Liam Wilson <cosinusoidally@gmail.com>
2024-12-16 07:38:52 +0000

    more

--- tcc_boot_min/tcc_boot_min.c
+++ tcc_boot_min/tcc_boot_min.c
@@ -843,6 +843,10 @@ int ELFW_R_INFO(int sym, int type) {
    return add(shl(sym, 8), and(type, 255));
 }
 
+int ELFW_R_SYM(int val) {
+  return and(shr(val,8), 16777215); /* unsigned ((val) >> 8) */
+}
+
 int ELFW_R_TYPE(int val) {
   return and(val,255);
 }
