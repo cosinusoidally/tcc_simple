af9795e50fddd7f0a1b77e53a1a78e1188b8c2c1
Liam Wilson <cosinusoidally@gmail.com>
2024-12-17 00:03:07 +0000

    more

--- tcc_boot_min/tcc_boot_min.c
+++ tcc_boot_min/tcc_boot_min.c
@@ -59,6 +59,9 @@ int ss_sh_type(int o, int v) {wi32(add(o, Section_sh_type_o), v);}
 int gs_sh_flags(int o) {return ri32(add(o, Section_sh_flags_o));}
 int ss_sh_flags(int o, int v) {wi32(add(o, Section_sh_flags_o), v);}
 
+int gs_sh_info(int o) {return ri32(add(o, Section_sh_info_o));}
+int ss_sh_info(int o, int v) {wi32(add(o, Section_sh_info_o), v);}
+
 int gs_sh_addralign(int o) {return ri32(add(o, Section_sh_addralign_o));}
 int ss_sh_addralign(int o, int v) {wi32(add(o, Section_sh_addralign_o), v);}
 
