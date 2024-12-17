7e29f132f7a9ea36722877c99457c4cdb1ec2f67
Liam Wilson <cosinusoidally@gmail.com>
2024-12-17 17:41:51 +0000

    more

--- tcc_boot_min/tccelf.c
+++ tcc_boot_min/tccelf.c
@@ -63,7 +63,7 @@ void sort_syms(TCCState *s1, Section *s) {
         if (eq(ELFW_ST_BIND(ges_st_info(p)), STB_LOCAL)) {
             old_to_new_syms[i] = q - new_syms;
             *q = *p;
-            q = q + 1;
+            q = add(q, sizeof_Elf32_Sym);
         }
         p = add(p, sizeof_Elf32_Sym);
         i = add(i, 1);
