826565c9f274cab07a035d5d047509cbb6371105
Liam Wilson <cosinusoidally@gmail.com>
2024-12-19 18:52:35 +0000

    more

--- tcc_boot_min/tccpp.c
+++ tcc_boot_min/tccpp.c
@@ -81,9 +81,10 @@ static inline void TOK_GET(int *t, const int **pp, CValue *cv) {
     *t = *p;
     p = add(p, 4);
     if((t == TOK_STR) || (t == TOK_PPNUM) || (t == TOK_PPSTR)) {
-        cv->str.size = *p++;
+        cv->str.size = *p;
+        p = add(p, 4);
         cv->str.data = p;
-        p += (cv->str.size + sizeof_int - 1) / sizeof_int;
+        p += div_(sub(add(cv->str.size, sizeof_int), 1), sizeof_int);
     }
 
     wi32(pp, p);
