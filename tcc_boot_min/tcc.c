9fa75613a6d526413d6835283b65c6766e3d9e37
Liam Wilson <cosinusoidally@gmail.com>
2024-12-20 18:33:28 +0000

    more

--- tcc_boot_min/tcc.c
+++ tcc_boot_min/tcc.c
@@ -21,7 +21,37 @@
 #include "tcc.h"
 #include "tccgen.c"
 
-#include "tccpp.c"
+static TokenSym *hash_ident[TOK_HASH_SIZE];
+static char token_buf[STRING_MAX_SIZE + 1];
+static CString cstr_buf;
+static unsigned char isidnum_table[256 - CH_EOF];
+
+extern TokenString *macro_stack;
+
+/* LJW HACK addreses of these variables */
+/* actually one or two are not addreses */
+extern int aTOK_HASH_SIZE;
+extern int ahash_ident;
+extern int acstr_buf;
+extern int CH_EOF_;
+extern int aisidnum_table;
+extern int atokc;
+extern int atokcstr;
+extern int atoken_buf;
+extern int avstack;
+
+int init_tccpp_globals(){
+  aTOK_HASH_SIZE = TOK_HASH_SIZE;
+  ahash_ident = hash_ident;
+  acstr_buf = &cstr_buf;
+  CH_EOF_ = CH_EOF;
+  aisidnum_table = isidnum_table;
+  atokc = &tokc;
+  atokcstr = &tokcstr;
+  atoken_buf = &token_buf;
+  avstack = vstack;
+  aSTRING_MAX_SIZE = STRING_MAX_SIZE;
+}
 
 int init_globals() {
     aglobal_stack = &global_stack;
