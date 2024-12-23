3d976fdf571a8ba17a659d9544f4a1c6d09a9f44
Liam Wilson <cosinusoidally@gmail.com>
2024-12-23 12:46:32 +0000

    more

--- tcc_boot_min/tcc_boot_min.c
+++ tcc_boot_min/tcc_boot_min.c
@@ -336,6 +336,39 @@ int gad_section(int o) {return ri32(add(o, AttributeDef_section_o));}
 
 /* stuff */
 
+int init_globals() {
+  aTOK_HASH_SIZE = 16384;     /* must be a power of two */
+
+  CH_EOF_ = sub(0, 1);
+
+  aSTRING_MAX_SIZE = 1024;
+
+  aglobal_stack = tcc_mallocz(sizeof_void); /* Sym *global_stack */
+  alocal_stack = tcc_mallocz(sizeof_void); /* Sym *local_stack */
+
+  aint_type = tcc_mallocz(sizeof_CType);
+  afunc_old_type = tcc_mallocz(sizeof_CType);
+  afunc_vt = tcc_mallocz(sizeof_CType);
+  asym_pools = tcc_mallocz(sizeof_void); /* void **sym_pools */
+  anb_sym_pools = tcc_mallocz(sizeof_int); /* int nb_sym_pools */
+
+  /* TokenSym *hash_ident[TOK_HASH_SIZE] */
+  ahash_ident = tcc_mallocz(mul(aTOK_HASH_SIZE, 4));
+  acstr_buf = tcc_mallocz(sizeof_CString);
+
+  /* unsigned char isidnum_table[256 - CH_EOF] */
+  aisidnum_table = tcc_mallocz(sub(256, CH_EOF_));
+  atokc = tcc_mallocz(sizeof_CValue);
+  atokcstr = tcc_mallocz(sizeof_CString);
+
+  /* char token_buf[STRING_MAX_SIZE + 1] */
+  atoken_buf = tcc_mallocz(add(aSTRING_MAX_SIZE, 1));
+
+  /* VSTACK_SIZE 256 */
+  /* SValue vstack[VSTACK_SIZE] */
+  avstack = tcc_mallocz(mul(256, sizeof_SValue));
+}
+
 int init_runtime(){
   foo=mks("hello world");
   puts(foo);
