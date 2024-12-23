7a4d871f07598032b57bd81222a32dd13d8dcc5f
Liam Wilson <cosinusoidally@gmail.com>
2024-12-23 12:28:17 +0000

    more

--- tcc_boot_min/tcc.c
+++ tcc_boot_min/tcc.c
@@ -1,23 +1,27 @@
 #include "tcc.h"
 
 int init_globals() {
-  aglobal_stack = &global_stack;
-  alocal_stack = &local_stack;
-
-  aint_type = &int_type;
-  afunc_old_type = &func_old_type;
-  afunc_vt = &func_vt;
-  asym_pools = &sym_pools;
-  anb_sym_pools = &nb_sym_pools;
-
   aTOK_HASH_SIZE = TOK_HASH_SIZE;
+
+  CH_EOF_ = sub(0, 1);
+
+  aSTRING_MAX_SIZE = STRING_MAX_SIZE;
+
+  aglobal_stack = tcc_mallocz(sizeof_void); /* Sym *global_stack */
+  alocal_stack = tcc_mallocz(sizeof_void); /* Sym *local_stack */
+
+  aint_type = tcc_mallocz(sizeof_CType);
+  afunc_old_type = tcc_mallocz(sizeof_CType);
+  afunc_vt = tcc_mallocz(sizeof_CType);
+  asym_pools = tcc_mallocz(sizeof_void); /* void **sym_pools */
+  anb_sym_pools = tcc_mallocz(sizeof_int); /* int nb_sym_pools */
+
+  /* TokenSym *hash_ident[TOK_HASH_SIZE] */
   ahash_ident = hash_ident;
   acstr_buf = &cstr_buf;
-  CH_EOF_ = CH_EOF;
   aisidnum_table = isidnum_table;
   atokc = &tokc;
   atokcstr = &tokcstr;
   atoken_buf = &token_buf;
   avstack = vstack;
-  aSTRING_MAX_SIZE = STRING_MAX_SIZE;
 }
