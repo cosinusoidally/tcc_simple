534ea1381aff95a3a56051ddd74d4177903a25b6
Liam Wilson <cosinusoidally@gmail.com>
2024-12-23 10:23:18 +0000

    more

--- tcc_boot_min/tcc.h
+++ tcc_boot_min/tcc.h
@@ -647,3 +647,75 @@ extern int TOK_ELSE;
 extern int TOK_WHILE;
 extern int TOK_BREAK;
 extern int TOK_RETURN;
+
+static TokenSym *hash_ident[TOK_HASH_SIZE];
+static char token_buf[STRING_MAX_SIZE + 1];
+static CString cstr_buf;
+static unsigned char isidnum_table[256 - CH_EOF];
+
+extern TokenString *macro_stack;
+
+/* LJW HACK addreses of these variables */
+/* actually one or two are not addreses */
+extern int aTOK_HASH_SIZE;
+extern int ahash_ident;
+extern int acstr_buf;
+extern int CH_EOF_;
+extern int aisidnum_table;
+extern int atokc;
+extern int atokcstr;
+extern int atoken_buf;
+extern int avstack;
+
+/********************************************************/
+/* global variables */
+
+/* loc : local variable index
+   ind : output code index
+   rsym: return symbol
+   anon_sym: anonymous symbol index
+*/
+extern int ind;
+
+extern Sym *sym_free_first;
+ST_DATA void **sym_pools;
+ST_DATA int nb_sym_pools;
+
+ST_DATA Sym *global_stack;
+ST_DATA Sym *local_stack;
+extern int local_scope;
+extern int section_sym;
+
+ST_DATA int global_expr;  /* true if compound literals must be allocated globally (used during initializers parsing */
+ST_DATA CType func_vt; /* current function return type (used by return instruction) */
+ST_DATA int func_vc;
+ST_DATA int func_ind;
+
+ST_DATA CType func_old_type;
+ST_DATA CType int_type;
+
+/* ------------------------------------------------------------------------- */
+
+void gen_cast(CType *type);
+static void gen_cast_s(int t);
+static inline CType *pointed_type(CType *type);
+static int is_compatible_types(CType *type1, CType *type2);
+static CType *type_decl(CType *type, AttributeDef *ad, int *v);
+static void init_putv(CType *type, Section *sec, unsigned long c);
+int decl_initializer(CType *type, Section *sec, unsigned long c, int first, int size_only);
+int block(int *bsym, int *csym, int is_expr);
+int decl_initializer_alloc(CType *type, AttributeDef *ad, int r, int has_init, int v, int scope);
+void decl(int l);
+int decl0(int l, int is_for_loop_init, Sym *);
+static void expr_eq(void);
+static int is_compatible_unqualified_types(CType *type1, CType *type2);
+static void vpush(CType *type);
+static int gvtst(int inv, int t);
+static void skip_or_save_block(TokenString **str);
+static void gv_dup(void);
+
+extern int aint_type;
+extern int afunc_old_type;
+extern int afunc_vt;
+extern int asym_pools;
+extern int anb_sym_pools;
