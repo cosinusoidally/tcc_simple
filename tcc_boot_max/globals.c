d38ab5132570edd9e2b0c633a4ec6757ea7a9b43
Liam Wilson <cosinusoidally@gmail.com>
2025-01-09 22:18:16 +0000

    more

--- tcc_boot_max/globals.c
+++ tcc_boot_max/globals.c
@@ -4,4 +4,7 @@ int ind;
 
 int cur_text_section;
 
+int Section_data_allocated_o;
+int sizeof_Section;
+
 /* =============== end globals.c ========================================== */
--- tcc_boot_max/tcc_boot_max.c
+++ tcc_boot_max/tcc_boot_max.c
@@ -1,10 +1,17 @@
 /* tcc_boot_max */
 
+/* Section accessors */
+int gs_data_allocated(int o) {return ri32(add(o, Section_data_allocated_o));}
+
 int init_globals() {
   puts("init_globals");
 }
 
 int init_runtime(){
   puts("init_runtime");
+
+  Section_data_allocated_o = 8;
+  sizeof_Section = 76;
+
   init_globals();
 }
