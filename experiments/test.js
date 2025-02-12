a5d33317387c0311cf7dee7976c1dac0979242db
Liam Wilson <cosinusoidally@gmail.com>
2025-02-12 21:44:22 +0000

    test dlopen and dlsym

--- experiments/test.js
+++ experiments/test.js
@@ -1,1 +1,15 @@
+function dlopen(x,y) {
+  return ffi(_dlopen, str_adr(x), y);
+}
+
+function dlsym(x,y) {
+  return ffi(_dlsym, x, str_adr(y));
+}
+
+libc=dlopen("libc.so.6", RTLD_LAZY);
+
+_puts=dlsym(libc, "puts");
+
+print("got here");
+
 ffi(_puts,str_adr("Hello world"));
