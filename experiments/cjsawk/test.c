bba3c87c21237d5fb4dff8dadeb3ded17ee7a328
Liam Wilson <cosinusoidally@gmail.com>
2025-05-26 15:08:47 +0100

    more

---
+++ experiments/cjsawk/test.c
@@ -0,0 +1,5 @@
+var a;
+
+function foo() {
+  return 1;
+}
--- experiments/cjsawk/token.js
+++ experiments/cjsawk/token.js
@@ -1,1 +1,40 @@
 f=read("test.c","binary");
+
+to=0;
+ts=0;
+te=0;
+ch=f[0];
+
+ln=1;
+
+function nc() {
+  ch=f[to];
+  to=to+1;
+}
+
+function is_whitespace() {
+  if((ch == 32) || (ch == 9)) {
+    return true;
+  }
+
+  if(ch == 10) {
+    ln = ln + 1;
+    return true;
+  }
+
+  return false;
+}
+
+function eat_whitespace() {
+  while(is_whitespace()) {
+    nc();
+  }
+}
+
+nc();
+
+while(to < f.length) {
+  eat_whitespace();
+  print(ln+ " " + ch + " " +String.fromCharCode(ch));
+  nc();
+}
