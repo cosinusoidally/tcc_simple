8b6eb492eeb01ad630f8fb46440e21582d238cbc
Liam Wilson <cosinusoidally@gmail.com>
2025-07-05 21:16:46 +0100

    more

--- experiments/cjsawk/support_tcc.c
+++ experiments/cjsawk/support_tcc.c
@@ -5,3 +5,23 @@ function mkc(c) {
 function mks(s) {
   return s;
 }
+
+function ri8(o) {
+  puts("ri8 not impl");
+}
+
+function wi8(o, v) {
+  puts("wi8 not impl");
+}
+
+function v_fgetc(f) {
+  return fgetc(f);
+}
+
+function v_calloc(a, b) {
+  return calloc(a, b);
+}
+
+function v_free(a) {
+  free(a);
+}
