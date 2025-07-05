e55d0202a9ae86029a9c8d813c30f7565a4adb2c
Liam Wilson <cosinusoidally@gmail.com>
2025-07-05 20:57:20 +0100

    split support

---
+++ experiments/cjsawk/support_alu.js
@@ -0,0 +1,95 @@
+function add(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a + b;
+}
+
+function sub(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a - b;
+}
+
+function mul(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a * b;
+}
+
+function shl(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a << b) | 0;
+}
+
+function shr(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a >>> b;
+}
+
+function lt(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a < b) | 0;
+}
+
+function gt(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a > b) | 0;
+}
+
+function lte(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a <= b) | 0;
+}
+
+function gte(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a >= b) | 0;
+}
+
+function and(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a & b;
+}
+
+function or(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a | b;
+}
+
+function eq(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a == b) | 0;
+}
+
+function neq(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a != b) | 0;
+}
+
+function xor(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a ^ b;
+}
+
+function div(a, b){
+  a = a | 0;
+  b = b | 0;
+  return (a / b) | 0;
+}
+
+function mod(a, b){
+  a = a | 0;
+  b = b | 0;
+  return a % b;
+}
