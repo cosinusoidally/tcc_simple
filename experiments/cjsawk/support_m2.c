2d3d1d819306898545890bfe9e776691e8f24477
Liam Wilson <cosinusoidally@gmail.com>
2025-07-06 19:48:12 +0100

    more

--- experiments/cjsawk/support_m2.c
+++ experiments/cjsawk/support_m2.c
@@ -1,3 +1,39 @@
-function init_runtime() {
+function mkc(c) {
+  return c;
+}
 
+function mks(s) {
+  return s;
+}
+
+function v_fopen(a, b) {
+ return fopen(a, b);
+}
+
+function v_fgetc(f) {
+  return fgetc(f);
+}
+
+function v_fputs(s, f) {
+  return fputs(s, f);
+}
+
+function v_fclose(f) {
+  return fclose(f);
+}
+
+function v_calloc(a, b) {
+  return calloc(a, b);
+}
+
+function v_free(a) {
+  free(a);
+}
+
+function v_exit(v) {
+  exit(v);
+}
+
+function init_runtime() {
+/* dummy */
 }
