7d930e34dc3730cda66c5074fedf35a68c346b40
Liam Wilson <cosinusoidally@gmail.com>
2025-07-26 18:50:18 +0100

    more

---
+++ experiments/cjsawk/common.sh
@@ -0,0 +1,9 @@
+export PATH=$PWD/artifacts/deps/:$PATH
+
+function compile() {
+  $1 $2 $3.M1
+  cat ../m2min_v3/simple_asm_defs.M1 ../m2min_v3/x86_defs.M1 ../m2min_v3/libc-core.M1 $3.M1 > $3-0.M1
+  M0 $3-0.M1 $3.hex2
+  cat ../m2min_v3/ELF-i386.hex2 $3.hex2 > $3-0.hex2
+  hex2-0 $3-0.hex2 $3
+}
