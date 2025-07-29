6c1b4c0b6a7e0a1ec12b21b793f48cf25d30a4e2
Liam Wilson <cosinusoidally@gmail.com>
2025-07-29 18:02:18 +0100

    more

--- experiments/cjsawk/cjsawk_web.js
+++ experiments/cjsawk/cjsawk_web.js
@@ -1,3 +1,44 @@
 window.onload=function() {
   console.log("cjsawk hello world");
+  getfile("cjsawk.js");
+  setTimeout(check_ready, 100);
 }
+
+function go() {
+  load("cjsawk.js");
+}
+
+function check_ready() {
+  var ready = true;
+  for(var i in vfs) {
+    if(vfs[i].ready == false){
+      ready=false;
+    }
+  }
+  if(ready) {
+    console.log("ready now");
+    go();
+  } else {
+    console.log("not ready");
+    setTimeout(check_ready, 100);
+  }
+}
+
+vfs ={};
+
+function getfile(f) {
+  var xhttp = new XMLHttpRequest();
+  var fd = {};
+  fd.ready = false;
+  vfs[f] = fd;
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    // Typical action to be performed when the document is ready:
+    fd.response = xhttp.responseText;
+    fd.ready = true;
+  }
+  };
+  xhttp.open("GET", f, true);
+  xhttp.send();
+}
+
