f0308eaa19cf0ba75b4003c0f9805f183504ad35
Liam Wilson <cosinusoidally@gmail.com>
2025-08-11 22:12:05 +0100

    more

--- experiments/cjsawk/m0.c
+++ experiments/cjsawk/m0.c
@@ -649,7 +649,6 @@ int main(int argc, char **argv) {
 
 	token_list = reverse_list(token_list);
 	line_macro(token_list);
-	process_string(blob_list);
 	eval_immediates(blob_list);
 	preserve_other(blob_list);
 	print_hex(token_list);
