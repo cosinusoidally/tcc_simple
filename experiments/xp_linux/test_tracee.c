08b4872faaeff48d25081c8c44d4e0cb36b1a07c
Liam Wilson <cosinusoidally@gmail.com>
2025-09-23 14:43:27 +0100

    mmap our heap

--- experiments/xp_linux/test_tracee.c
+++ experiments/xp_linux/test_tracee.c
@@ -2,6 +2,13 @@ extern int stdout;
 
 int regs_data[8];
 
+int PROT_READ=1;
+int PROT_WRITE=2;
+int PROT_EXEC=4;
+int MAP_ANONYMOUS=32;
+int MAP_PRIVATE=2;
+int MAP_FIXED=0x10;
+
 int wrap_syscall2() {
   printf("wrap_syscall2 called\n");
   return 0;
@@ -29,6 +36,14 @@ int wrap_syscall() {
 main(){
   char *a="Test syscall\n";
   char *b="test_trap blah\n";
+/* big mapping for our heap */
+  int res = 0;
+  res = mmap(0x20000, 7*512*1024*1024, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, 0, 0);
+  printf("res: %d\n", res);
+  if(res == -1 ) {
+    printf("mmap error\n");
+    exit(1);
+  }
   fputs("hello world\n", stdout);
   printf("a: %d\n",a);
   printf("stdout: %d\n", stdout);
