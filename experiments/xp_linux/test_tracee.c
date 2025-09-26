3e5cd44745d8bce29ca52806558863ff55e74c3f
Liam Wilson <cosinusoidally@gmail.com>
2025-09-26 23:35:48 +0100

    more

--- experiments/xp_linux/test_tracee.c
+++ experiments/xp_linux/test_tracee.c
@@ -108,8 +108,11 @@ int vm_write() {
     printf("vm_read only supports count 1\n");
     exit(1);
   }
-  wi8(gfd_get_file_addr(fd_get_filenum(fd))+fd_get_file_offset(fd), ri8(buf));
-  fd_set_file_offset(fd, fd_get_file_offset(fd) + 1);
+  t = fd_get_file_offset(fd);
+  wi8(gfd_get_file_addr(fd_get_filenum(fd))+t, ri8(buf));
+  t = t + 1;
+  fd_set_file_offset(fd, t);
+  gfd_set_file_length(fd_get_filenum(fd), t);
   return r;
 }
 
