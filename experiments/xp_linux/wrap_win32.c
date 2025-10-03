9e5271bec86831f86106b65528f407a7b0856a02
Liam Wilson <cosinusoidally@gmail.com>
2025-10-03 11:02:36 +0100

    more

--- experiments/xp_linux/wrap_win32.c
+++ experiments/xp_linux/wrap_win32.c
@@ -7,7 +7,10 @@ int syscall(x) {
 }
 
 int mmap(base, size) {
+  int r;
   printf("mmap not impl base: 0x%x size: 0x%x\n", base, size);
+  r = VirtualAlloc((LPVOID)base, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+  printf("r: 0x%x\n",r);
   exit(1);
 }
 
