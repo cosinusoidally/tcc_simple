f2c8e4c8186554d5e27ebf5790763a2f54ae2ae2
Liam Wilson <cosinusoidally@gmail.com>
2025-10-03 10:59:33 +0100

    dummy mmap and syscall impl

--- experiments/xp_linux/wrap_win32.c
+++ experiments/xp_linux/wrap_win32.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 #include <windows.h>
 
+int syscall(x) {
+  printf("sycall function not impl %d\n", x);
+  exit(1);
+}
+
+int mmap() {
+  printf("mmap not impl\n");
+  exit(1);
+}
+
 #include "test_tracee.c"
