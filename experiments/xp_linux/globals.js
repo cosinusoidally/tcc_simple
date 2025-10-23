7ada740e23b409f39fee5dc49032c70f63bbcd4d
Liam Wilson <cosinusoidally@gmail.com>
2025-10-23 14:38:58 +0100

    more

--- experiments/xp_linux/globals.js
+++ experiments/xp_linux/globals.js
@@ -1,3 +1,7 @@
 function elf_base() {
   return 0x8048000;
 }
+
+function base_address() {
+  return 0x4000000;
+}
--- experiments/xp_linux/min_linux.c
+++ experiments/xp_linux/min_linux.c
@@ -14,7 +14,6 @@ int MAP_ANONYMOUS=32;
 int MAP_PRIVATE=2;
 int MAP_FIXED=0x10;
 
-int base_address;
 int data_area;
 int host_call_fn;
 int host_params;
@@ -104,8 +103,7 @@ int load_boot(filename) {
 }
 
 init_globals() {
-  base_address = 0x4000000; /* 64MB */
-  data_area = base_address+0x20000;
+  data_area = base_address() +0x20000;
   regs_data = data_area;
   host_call_fn = data_area + (4*9);
   host_params = host_call_fn + 4;
@@ -167,8 +165,8 @@ run_process() {
 int main(int argc, char **argv) {
   int res = 0;
   init_globals();
-  res = mmap(base_address, 512*1024*1024, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, 0, 0);
-  if(res != base_address) {
+  res = mmap(base_address(), 512*1024*1024, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, 0, 0);
+  if(res != base_address()) {
     puts("mmap error\n");
     exit(1);
   }
