18fe216537b518776641bee6c971908545aa588b
Liam Wilson <cosinusoidally@gmail.com>
2025-11-16 09:47:55 +0000

    more

--- experiments/cjsawk/cjsawk_xpcshell.js
+++ experiments/cjsawk/cjsawk_xpcshell.js
@@ -9,6 +9,47 @@ libc = ctypes.open("libc.so.6");
 
 puts = libc.declare("puts", ctypes.default_abi, ctypes.int32_t, ctypes.char.ptr);
 
+// FILE * fopen ( const char * filename, const char * mode );
+var fopen=libc.declare("fopen",
+                        ctypes.default_abi, /* call ABI */
+                        ctypes.voidptr_t,         /* return type */
+                        ctypes.char.ptr,  /* argument type */
+                        ctypes.char.ptr);   /* argument type */
+// int fseek(FILE *stream, long offset, int whence);
+var fseek=libc.declare("fseek",
+                        ctypes.default_abi, /* call ABI */
+                        ctypes.int,         /* return type */
+                        ctypes.voidptr_t,  /* argument type */
+                        ctypes.long,   /* argument type */
+                        ctypes.int);   /* argument type */
+// void rewind(FILE *stream);
+var rewind=libc.declare("rewind",
+                        ctypes.default_abi, /* call ABI */
+                        ctypes.void_t,
+                        ctypes.voidptr_t);
+var SEEK_END=2;
+// int fclose ( FILE * stream );
+var fclose=libc.declare("fclose",
+                        ctypes.default_abi, /* call ABI */
+                        ctypes.int,
+                        ctypes.voidptr_t);
+
+// long int ftell ( FILE * stream );
+var ftell=libc.declare("ftell",
+                        ctypes.default_abi, /* call ABI */
+                        ctypes.long,
+                        ctypes.voidptr_t);
+
+// size_t fread(void *ptr, size_t size, size_t nmemb, FILE * stream );
+var fread=libc.declare("fread",
+                        ctypes.default_abi, /* call ABI */
+                        ctypes.int,
+                        ctypes.voidptr_t,
+                        ctypes.int,
+                        ctypes.int,
+                        ctypes.voidptr_t);
+
+
 print(ctypes);
 print(puts);
 puts("hello world from ctypes");
