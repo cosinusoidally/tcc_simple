04516ab3624578551c1ad2314ab85ce492133804
Liam Wilson <cosinusoidally@gmail.com>
2025-11-17 14:04:35 +0000

    fix stdout and stderr for firefox xpcshell mode on win32

--- experiments/cjsawk/mk_xpcshell.ps1
+++ experiments/cjsawk/mk_xpcshell.ps1
@@ -1,6 +1,10 @@
 rm -r artifacts/builds/xpcshell
 mkdir artifacts/builds
 mkdir artifacts/builds/xpcshell
-../../../firefox/firefox.exe -xpcshell cjsawk_xpcshell.js
+# work around https://bugzilla.mozilla.org/show_bug.cgi?id=1807043 by
+# redirecting stdout and and stderr
+../../../firefox/firefox.exe -xpcshell cjsawk_xpcshell.js *>&1 | ForEach-Object { Write-Host "$_" }
+
+Get-FileHash -A sha1 artifacts/builds/xpcshell/*
 
 echo "DONE xpcshell"
