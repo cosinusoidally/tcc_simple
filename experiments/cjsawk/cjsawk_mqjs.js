d3b681d47587a0780707057c624ce043d4d2b5d3
Liam Wilson <cosinusoidally@gmail.com>
2026-01-02 12:28:15 +0000

    more

--- experiments/cjsawk/cjsawk_mqjs.js
+++ experiments/cjsawk/cjsawk_mqjs.js
@@ -13,4 +13,22 @@ var mks;
 
 var argv;
 
+var fname;
+
+fname = "artifacts/deps/cjsawk_full.c";
+
+var read_=read;
+
+read=function(x,y){
+  if(arguments.length>1){
+    if(y==="binary"){
+      var b=read_(x);
+      b=b.split("");
+      b=b.map(function(x) {return x.charCodeAt(0);});
+      return b;
+    };
+  };
+  return read_(x);
+};
+
 load("cjsawk_test.js");
