333ac958c4b39338d993968ac901af7ed7c6d1a7
Liam Wilson <cosinusoidally@gmail.com>
2026-09-05 19:02:43 +0100

    more

--- experiments/cjsawk/csharp.js
+++ experiments/cjsawk/csharp.js
@@ -5,5 +5,26 @@ f=fs.readFileSync("cjsawk.js","utf-8");
 f=f.split("\n");
 
 for(i=0;i<f.length;i++) {
-  console.log(f[i]);
+  var t;
+  var to;
+  var o;
+  to = t;
+  t = f[i].split(" ");
+  to = t.join(" ");
+  if(t[0]==="function") {
+    t.shift();
+    t="static int "+t.join(" ");
+    t=t.split("(");
+    t2=t[1].split(")")[0];
+    t2=t2.split(",");
+    t2=t2.map(function(x){x=x.split(" ").join(""); return "int "+x});
+    t = t[0] +"("+t2.join(", ")+") {";
+    console.log("----");
+    console.log(to);
+    console.log(t);
+  } else if(t[0]==="var") {
+    t.shift();
+    t="static int "+t.join(" ");
+    console.log(t);
+  }
 }
