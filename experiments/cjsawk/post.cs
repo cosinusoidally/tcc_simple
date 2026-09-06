a7c91e5ee37d82d2cb6ec1079359c4096af8b2a1
Liam Wilson <cosinusoidally@gmail.com>
2026-09-06 12:39:46 +0100

    set console encoding

--- experiments/cjsawk/post.cs
+++ experiments/cjsawk/post.cs
@@ -1,6 +1,7 @@
   public static int Main(string[] args) {
 //    Console.WriteLine("Hello world");
 //    not_impl("blah");
+    Console.OutputEncoding=Encoding.UTF8;
     init_cs();
     int cmd=mks("cjsawk.exe");
     int inp=mks(args[0]);
