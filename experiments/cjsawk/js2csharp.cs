9af8e1adcdec5ab1b4689abee5e5eb1ae98b56af
Liam Wilson <cosinusoidally@gmail.com>
2026-09-09 12:13:33 +0100

    more

--- experiments/cjsawk/js2csharp.cs
+++ experiments/cjsawk/js2csharp.cs
@@ -5,6 +5,17 @@ using System.Text;
 public class Program
 {
   public static void Main(string[] args) {
+    string[] o = {"\n", "\n", "\n"};
+    if(args.Length!=2) {
+      throw new Exception("must give 2 args");
+    }
+    o[0]=File.ReadAllText("pre.cs");
+    o[2]=File.ReadAllText("post.cs");
+    Console.Write(String.Join("\n", o));
+/*
+    fs.Write(pre);
+    fs.Close();
+*/
     Console.WriteLine("hello  world");
   }
 }
