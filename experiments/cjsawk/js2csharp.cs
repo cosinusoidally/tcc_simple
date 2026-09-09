6224b0da818edad072a3be544613cc0acc11401f
Liam Wilson <cosinusoidally@gmail.com>
2026-09-09 12:27:48 +0100

    more

--- experiments/cjsawk/js2csharp.cs
+++ experiments/cjsawk/js2csharp.cs
@@ -18,6 +18,21 @@ public class Program
     string[] ta;
     ta = t.Split("\n");
 
+    for(int i=0;i<ta.Length;i++) {
+      string[] tl;
+      string ts;
+      tl=ta[i].Split(" ");
+      if(tl[0]=="function") {
+        tl[0] = "static int";
+        ts = String.Join(" ", tl);
+        tl = ts.Split("(");
+        string t2 = tl[1].Split(")")[0];
+        t2 = String.Join("", t2.Split(" "));
+        ta[i] = ts + "// "+t2;
+      } else {
+      }
+    }
+
     t = String.Join("\n",ta);
     o[1] = t;
     f= String.Join("\n", o);
