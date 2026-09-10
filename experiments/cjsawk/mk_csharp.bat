2a86cf4af5522e5fc7408450380d8f8f399b5fd2
Liam Wilson <cosinusoidally@gmail.com>
2026-09-10 10:57:39 +0100

    csc wants the out flag before the input filename

--- experiments/cjsawk/mk_csharp.bat
+++ experiments/cjsawk/mk_csharp.bat
@@ -5,7 +5,7 @@ del artifacts\cjsawk.M1
 
 set CSC_PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 
-%CSC_PATH% js2csharp.cs /out:artifacts\js2csharp.exe
+%CSC_PATH% /out:artifacts\js2csharp.exe js2csharp.cs
 .\artifacts\js2csharp.exe cjsawk_csharp.cs artifacts\cjsawk.cs
-%CSC_PATH% artifacts\csharp.cs /out:artifacts\cjsawk.exe
+%CSC_PATH% /out:artifacts\cjsawk.exe artifacts\cjsawk.cs
 .\artifacts\cjsawk.exe cjsawk.js artifacts\cjsawk.M1
