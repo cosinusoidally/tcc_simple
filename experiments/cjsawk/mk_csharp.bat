8831d954f238d730a0eedd3252ca9ee37b8e1479
Liam Wilson <cosinusoidally@gmail.com>
2026-09-11 13:07:56 +0100

    fix typo

--- experiments/cjsawk/mk_csharp.bat
+++ experiments/cjsawk/mk_csharp.bat
@@ -6,6 +6,6 @@ del artifacts\cjsawk.M1
 set CSC_PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
 
 %CSC_PATH% /out:artifacts\js2csharp.exe js2csharp.cs
-.\artifacts\js2csharp.exe cjsawk_csharp.cs artifacts\cjsawk.cs
+.\artifacts\js2csharp.exe cjsawk_csharp.js artifacts\cjsawk.cs
 %CSC_PATH% /out:artifacts\cjsawk.exe artifacts\cjsawk.cs
 .\artifacts\cjsawk.exe cjsawk.js artifacts\cjsawk.M1
